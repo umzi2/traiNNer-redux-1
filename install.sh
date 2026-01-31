8020b07379e16e4e8c652b4d48730068e39bacad
umzi <ab5447612@gmail.com>
2026-01-31 16:33:14 +0300

    fix install

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 uv venv --python 3.12
-source venv/bin/activate
 uv pip install -U torch torchvision --index-url https://download.pytorch.org/whl/cu128 --index-strategy unsafe-best-match --no-cache --link-mode=copy
 uv pip install . --index-strategy unsafe-best-match --link-mode=copy --no-cache
 
